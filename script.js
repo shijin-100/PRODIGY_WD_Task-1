162f43c3c2bc4fe2e287e368a4293179a40b6856
shijin-100 <shijin04072004@gmail.com>
2026-06-03 13:35:07 +0530

    index.html script.js style.css

---
+++ script.js
@@ -0,0 +1,11 @@
+// Select Navbar
+const navbar = document.getElementById("navbar");
+
+// Add Scroll Event
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
